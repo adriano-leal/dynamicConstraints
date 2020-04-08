0eb2c66506c9c2698cc577779c87bcc6446df447
adriano <adriano.ramos@realizecfi.com.br>
2020-04-08 09:13:14 -0300

    Initial Commit

---
+++ DynamicConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicConstraints
+//
+//  Created by Adriano Ramos on 08/04/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
